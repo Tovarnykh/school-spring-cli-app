759b9f4dabac189093c594742e6f95fd9de6a9b7
Viktor Tovarnykh <milaslavska2@gmail.com>
2023-01-12 13:04:58 +0100

    Project Initialization

---
+++ school-jdbc-api/src/main/java/ua/foxminded/javaspring/tovarnykh/school_jdbc_api/SchoolJdbcApiApplication.java
@@ -0,0 +1,13 @@
+package ua.foxminded.javaspring.tovarnykh.school_jdbc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolJdbcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolJdbcApiApplication.class, args);
+	}
+
+}
---
+++ school-jdbc-api/src/test/java/ua/foxminded/javaspring/tovarnykh/school_jdbc_api/SchoolJdbcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.foxminded.javaspring.tovarnykh.school_jdbc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolJdbcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
